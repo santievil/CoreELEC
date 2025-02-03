c38cd4554b7842ee08876a394f4adc50f980ee40
santievil <santi_evil@yahoo.com>
2025-02-03 21:37:32 +0100

    Añadido el addon HyperHDR a la rama coreelec-21

---
+++ packages/addons/service/hyperhdr/package.mk
@@ -0,0 +1,47 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="hyperhdr"
+PKG_VERSION="f1e847f8e4ad890fd00a9b85c750f474e2b00f9c"
+PKG_REV="205"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/awawa-dev/HyperHDR"
+PKG_URL="https://github.com/awawa-dev/HyperHDR.git"
+GET_HANDLER_SUPPORT="git"
+PKG_DEPENDS_TARGET="toolchain qt-everywhere pkg-config libjpeg-turbo alsa"
+PKG_TOOLCHAIN="cmake"
+PKG_SECTION="service"
+PKG_SHORTDESC="HyperHDR: an ambient lighting controller"
+PKG_LONGDESC="HyperHDR (v21.0.0.0) is an opensource ambient lighting implementation."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="HyperHDR"
+PKG_ADDON_TYPE="xbmc.service"
+
+# Setting default values
+PKG_PLATFORM="-DPLATFORM=x86"
+PKG_DENABLE_WS281XPWM="-DENABLE_WS281XPWM=0"
+
+if [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
+  PKG_PLATFORM="-DPLATFORM=linux"
+elif [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
+  PKG_PLATFORM="-DPLATFORM=rpi"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver rpi_ws281x"
+  PKG_DENABLE_WS281XPWM="-DENABLE_WS281XPWM=1"
+elif [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET xorg-server xrandr"
+fi
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
+                       -DCMAKE_BUILD_TYPE=Release \
+                       -DUSE_STATIC_QT_PLUGINS=ON \
+                       $PKG_PLATFORM \
+                       $PKG_DENABLE_WS281XPWM \
+                       -Wno-dev"
+
+addon() {
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib,lut}  
+  cp -r -P -p $(get_install_dir hyperhdr)/usr/share/hyperhdr/bin/* ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+  cp -r -P $(get_install_dir hyperhdr)/usr/share/hyperhdr/lib/* ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
+  tar -xf $(get_install_dir hyperhdr)/usr/share/hyperhdr/lut/lut_lin_tables.tar.xz -C ${ADDON_BUILD}/${PKG_ADDON_ID}/lut
+}
